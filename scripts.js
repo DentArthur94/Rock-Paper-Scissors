fbfc811d4589a5d70c613d224324ccf97d506fff
DentArthur94 <73761376+DentArthur94@users.noreply.github.com>
2025-01-29 08:58:38 +0100

    Feature:new ui

--- scripts.js
+++ scripts.js
@@ -6,30 +6,77 @@ function playGame() {
 
     let humanScore = 0;
     let computerScore = 0;
+    const choices = document.querySelector(".choices");
+    const humanScoreDiv = document.querySelector("#player-score");
+    const computerScoreDiv = document.querySelector("#computer-score");
+    const endGameMsg = document.querySelector(".end-game");
 
-    for (let i = 0 ; i < 5 ; i++) {
-        let roundWinner = playRound(getComputerChoice(),getHumanChoice());
+
+        choices.addEventListener("click", (event) => {
+            endGameMsg.textContent = ""
+            let target = event.target;
+            let roundWinner = playRound(getComputerChoice(),target.textContent);
+            switch (roundWinner) {
+                case "Computer":
+                computerScore++;
+                break;
+
+                case "Human":
+                humanScore++;
+                break;
+            }
+
+            computerScoreDiv.textContent = `Computer : ${computerScore}`;
+            humanScoreDiv.textContent = `Human : ${humanScore}`;
+
+            if (humanScore === 5) {
+                endGameMsg.textContent = "You are the winner ! Click any button to start a new game!";
+                humanScore = 0;
+                computerScore = 0;
+                return ;
+            } else if (computerScore === 5) {
+                endGameMsg.textContent = "You are the loser ! Click any button to start a new game!";
+                humanScore = 0;
+                computerScore = 0;
+                return ;
+            }
+        })
+
+
         
-        switch (roundWinner) {
-            case "Computer":
-            computerScore++;
-            break;
-
-            case "Human":
-            humanScore++;
-            break;
-        }
-
-    }
-
-    console.log(`Computer : ${computerScore}`);
-    console.log(`Human : ${humanScore}`);
 }
 
 
-function playRound(computerChoice,humanChoice) {
 
-    let roundWinner;
+
+function playRound(computerChoice,humanChoice) {
+    const humanChoiceLogo = document.querySelector("#player-choice-img");
+    const computerChoiceLogo = document.querySelector("#computer-choice-img");
+
+    switch(computerChoice) {
+        case("Rock"):
+            computerChoiceLogo.setAttribute("src","img/rock.png");
+            break;
+        case("Paper"):
+            computerChoiceLogo.setAttribute("src","img/paper.png");
+            break;
+        case("Scissors"):
+            computerChoiceLogo.setAttribute("src","img/scissors.png");
+            break;
+    }
+
+    switch(humanChoice) {
+        case("Rock"):
+            humanChoiceLogo.setAttribute("src","img/rock.png");
+            break;
+        case("Paper"):
+            humanChoiceLogo.setAttribute("src","img/paper.png");
+            break;
+        case("Scissors"):
+            humanChoiceLogo.setAttribute("src","img/scissors.png");
+            break;
+    }
+
 
     if (computerChoice === "Rock" && humanChoice === "Scissors" || computerChoice === "Scissors" && humanChoice === "Paper" || computerChoice === "Paper" && humanChoice === "Rock" ) {
         console.log(computerChoice,humanChoice);
@@ -47,35 +94,18 @@ function playRound(computerChoice,humanChoice) {
 }
 
 
-function mapChoice(choiceNumber) {
-    switch(choiceNumber) {
+function getComputerChoice() {
+
+    let randomNumber = Math.floor(Math.random() * 3);   //0,1,2
+    switch (randomNumber) {
         case 0:
             return 'Rock';
-
 
         case 1:
             return 'Paper';
 
-
         case 2:
             return 'Scissors';
     }
-}
-
-function getComputerChoice() {
-
-    let randomNumber = Math.floor(Math.random() * 3);   //0,1,2
-    return mapChoice(randomNumber);
-
     }
-
-
-function getHumanChoice() {
-
-    let humanNumber;
-    while (isNaN(humanNumber) || humanNumber < 0 || humanNumber > 2) {
-        humanNumber = Number(prompt("Choose : 0 for Rock, 1 for Paper or  2 for Scissors ?"));
-    }
-    return mapChoice(humanNumber);
-}
 
